d418ef3ec1e7745f76badf0b0d1b3cc285bb449d
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2023-10-20 23:44:31 -0300

    diego

--- gitReport/src/application/Program.java
+++ gitReport/src/application/Program.java
@@ -4,7 +4,11 @@ public class Program {
 
     public static void main(String[] args) {
 
+        //commit 1
         System.out.println("Hello World");
-        
+
+        //commit 2
+        System.out.println("Commit BRANCH Diego");
+
     }
 }
